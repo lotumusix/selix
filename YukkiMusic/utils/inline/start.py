081a873073e13c5b97eb22836a8d6c25b7952bf6
lotumusix <ratherom485@gmail.com>
2024-10-22 17:00:31 +0530

    Update start.py
    Signed-off-by: lotumusix <ratherom485@gmail.com>

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -68,7 +68,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                url=f"http://www.app.miwonsol.com",
             )
         ]
     )
